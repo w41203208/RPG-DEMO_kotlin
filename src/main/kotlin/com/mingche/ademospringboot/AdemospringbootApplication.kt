619b24ab205167f7dfced5486dd54107fb1a0ab7
w41203208@gamil.com <w41203208@gmail.com>
2022-06-27 00:49:08 +0800

    init project

---
+++ src/main/kotlin/com/mingche/ademospringboot/AdemospringbootApplication.kt
@@ -0,0 +1,11 @@
+package com.mingche.ademospringboot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AdemospringbootApplication
+
+fun main(args: Array<String>) {
+	runApplication<AdemospringbootApplication>(*args)
+}
