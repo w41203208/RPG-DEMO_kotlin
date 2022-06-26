22d99578355ca07747bd30db15b224a496f944e5
w41203208@gamil.com <w41203208@gmail.com>
2022-06-27 01:12:41 +0800

    ssl wrong and db connect

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-mustache")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	runtimeOnly("org.postgresql:postgresql")
+	runtimeOnly("mysql:mysql-connector-java")
+	runtimeOnly("com.microsoft.sqlserver:mssql-jdbc")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
