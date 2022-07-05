f3a0f76eac9be71928ff31dd7252cc0a505161bf
Jay <w41203208@gmail.com>
2022-07-05 10:17:35 +0800

    cors ok

--- src/main/kotlin/com/mingche/ademospringboot/AdemospringbootApplication.kt
+++ src/main/kotlin/com/mingche/ademospringboot/AdemospringbootApplication.kt
@@ -3,12 +3,15 @@ package com.mingche.ademospringboot
 import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
 class AdemospringbootApplication{
 }
+
 
 fun main(args: Array<String>) {
 	runApplication<AdemospringbootApplication>(*args){
